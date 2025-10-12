2ef5de877e18ef8a28a4c73e49b073157ebe54c6
0xMishra <mishrakundan073@gmail.com>
2025-10-13 02:18:51 +0530

    setup panic recovery mechanisms

---
+++ cmd/api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// API version
+const version = "1.0.0"
+
+// config struct to holds all the configuration settings for the API
+type config struct {
+	port int
+	env  string
+}
+
+// application struct to hold the dependencies for the HTTP handlers, helpers and middleware
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Using the httprouter instance returned by app.routes() as the server handler.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
+}
